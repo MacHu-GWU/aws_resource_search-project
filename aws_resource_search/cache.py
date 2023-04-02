6b43c8fbccc76f3e22bbba9888e2ac3da18311c2
Sanhe Hu <husanhe@gmail.com>
2023-04-02 10:20:50 -0400

    Update cache.py

--- aws_resource_search/cache.py
+++ aws_resource_search/cache.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from diskcache import Cache
+from pathlib_mate import Path
 
 from .paths import dir_cache
 
 dir_cache.parent.mkdir(parents=True, exist_ok=True)
 cache = Cache(dir_cache.abspath)
+
+
+def clear_cache():
+    Path(cache.directory).remove_if_exists()
