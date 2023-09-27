a5356f685f31b3cfe0ed406513f24a82aad45793
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-09-26 22:50:38 -0400

    Update search.py

--- scripts/search.py
+++ scripts/search.py
@@ -10,13 +10,14 @@ bsm = BotoSesManager(profile_name="edf_sbx_eu_west_1_mfa", region_name="eu-west-
 if __name__ == "__main__":
     ars = ARS(bsm=bsm)
 
-    query = "gesa 956"
+    query = "956"
     # refresh_data = True
     refresh_data = False
     limit = 5
-    # docs = ars.cloudformation_stack.query("gesa 956", limit=limit, refresh_data=refresh_data)
-    # docs = ars.ec2_instance.query("gesa", limit=limit, refresh_data=refresh_data)
-    docs = ars.s3_bucket.query("gesa 956", limit=5, refresh_data=refresh_data)
+    # docs = ars.cloudformation_stack.query(query, limit=limit, refresh_data=refresh_data)
+    # docs = ars.ec2_instance.query(query, limit=limit, refresh_data=refresh_data)
+    docs = ars.iam_role.query(query, limit=limit, refresh_data=refresh_data)
+    # docs = ars.s3_bucket.query(query, limit=5, refresh_data=refresh_data)
     print(f"number of docs: {len(docs)}")
     for doc in docs:
-        print(doc["name"], doc["console_url"])
+        print(doc["id"], doc["name"], doc["console_url"])
