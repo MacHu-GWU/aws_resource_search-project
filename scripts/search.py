af17cc22ef977ea5743532fe0a20a0e3755222c8
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-09-28 22:39:00 -0400

    chore

--- scripts/search.py
+++ scripts/search.py
@@ -11,29 +11,30 @@ if __name__ == "__main__":
     ars = ARS(bsm=bsm)
 
     query = "*"
-    # refresh_data = True
-    refresh_data = False
+    refresh_data = True
+    # refresh_data = False
     limit = 20
 
     # docs = ars.cloudformation_stack.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
-    # docs = ars.ec2_instance.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
+    docs = ars.ec2_instance.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
     # docs = ars.glue_database.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
     # docs = ars.glue_job.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
     # docs = ars.glue_crawler.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
     # docs = ars.iam_role.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
     # docs = ars.s3_bucket.search(query, limit=5, refresh_data=refresh_data, simple_response=True)
     # print(f"number of docs: {len(docs)}")
+    for doc in docs:
+        print(doc)
+        print(doc["id"], doc["name"], doc["console_url"], doc["raw_data"]["_out"]["subtitle"])
+
+    # docs = ars.glue_database.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
+    # db_name = docs[0]["name"]
+    # docs = ars.glue_table.search(
+    #     query,
+    #     limit=limit,
+    #     boto_kwargs={"DatabaseName": db_name},
+    #     refresh_data=refresh_data,
+    #     simple_response=True,
+    # )
     # for doc in docs:
     #     print(doc["id"], doc["name"], doc["console_url"])
-
-    docs = ars.glue_database.search(query, limit=limit, refresh_data=refresh_data, simple_response=True)
-    db_name = docs[0]["name"]
-    docs = ars.glue_table.search(
-        query,
-        limit=limit,
-        boto_kwargs={"DatabaseName": db_name},
-        refresh_data=refresh_data,
-        simple_response=True,
-    )
-    for doc in docs:
-        print(doc["id"], doc["name"], doc["console_url"])
