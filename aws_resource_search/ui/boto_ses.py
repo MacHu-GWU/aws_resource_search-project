12b5d05d6599bae90c2826ff360b0381f11e730c
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-11-01 22:33:28 -0400

    print debug information when boto session is malformed

--- aws_resource_search/ui/boto_ses.py
+++ aws_resource_search/ui/boto_ses.py
@@ -6,9 +6,30 @@ the default url. You could use the `awscli_mate <https://github.com/MacHu-GWU/aw
 tool to set any given profile as default.
 """
 
+import botocore.exceptions
 from boto_session_manager import BotoSesManager
 
 from ..ars import ARS
+from ..exc import MalformedBotoSessionError
 
 bsm = BotoSesManager()
 ars = ARS(bsm=bsm)
+
+# validate the boto session manager
+try:
+    _ = bsm.aws_account_id
+except Exception as e:  # pragma: no cover
+    raise MalformedBotoSessionError(
+        f"failed to use sts.get_caller_identity() to get AWS account id:, "
+        f"please check your default AWS profile in ~/.aws/config and ~/.aws/credentials, "
+        f"error: {e}"
+    )
+
+try:
+    _ = bsm.aws_region
+except Exception as e:  # pragma: no cover
+    raise MalformedBotoSessionError(
+        f"failed to get AWS region of your boto session, "
+        f"please check your default AWS profile in ~/.aws/config and ~/.aws/credentials, "
+        f"error: {e}"
+    )
