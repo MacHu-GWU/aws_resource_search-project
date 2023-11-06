3af3cc18d624252f2d2a7b0ec8299fb975724172
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-11-06 15:53:05 -0500

    chore

---
+++ count_code.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import typing as T
+import json
+import subprocess
+from pathlib_mate import Path
+
+dir_project_root = Path.dir_here(__file__).absolute()
+
+
+def cloc(path: T.Union[Path, T.List[Path]]) -> dict:
+    if isinstance(path, list):
+        cloc_list_file.write_text("\n".join([str(p) for p in path]))
+        args = [
+            "cloc",
+            f"--list-file={cloc_list_file}",
+            "--json",
+        ]
+    else:
+        args = [
+            "cloc",
+            f"{path}",
+            "--json",
+        ]
+    result = subprocess.run(args, capture_output=True)
+    data = json.loads(result.stdout.decode("utf-8"))
+    if "header" in data:
+        del data["header"]
+    return data
+
+
+def count_code(title, path: T.Union[Path, T.List[Path]]):
+    data = cloc(path)
+    print(f"-------------------- {title} --------------------")
+    print(json.dumps(data, indent=4))
+
+
+
+if __name__ == "__main__":
+    cloc_list_file = dir_project_root.joinpath(".cloc-list-file")
+
+    count_code(
+        "source code",
+        [
+            dir_project_root.joinpath("aws_resource_search"),
+        ],
+    )
+    count_code(
+        "test code",
+        [
+            dir_project_root.joinpath("tests"),
+        ],
+    )
+    count_code(
+        "ops code",
+        [
+            dir_project_root.joinpath("bin"),
+        ],
+    )
--- debug/debug_ars.py
+++ debug/debug_ars.py
@@ -17,8 +17,38 @@ if __name__ == "__main__":
     simple_response = True
     verbose = False
     # --------------------------------------------------------------------------
-    sr = ars.s3_bucket
+    # sr = ars.cloudformation_stack
+    # sr = ars.codebuild_job_run
+    # sr = ars.codebuild_project
+    # sr = ars.codecommit_repository
+    # sr = ars.codepipeline_pipeline
+    # sr = ars.dynamodb_table
+    # sr = ars.ec2_instance
+    # sr = ars.ec2_security_group
+    # sr = ars.ec2_subnet
+    # sr = ars.ec2_vpc
+    # sr = ars.glue_crawler
+    # sr = ars.glue_database
+    # sr = ars.glue_database_table
+    # sr = ars.glue_job
+    # sr = ars.glue_job_run
+    # sr = ars.iam_group
+    # sr = ars.iam_policy
     # sr = ars.iam_role
+    # sr = ars.iam_user
+    # sr = ars.kms_key_alias
+    # sr = ars.lambda_function
+    # sr = ars.lambda_function_alias
+    # sr = ars.lambda_layer
+    # sr = ars.rds_db_cluster
+    # sr = ars.rds_db_instance
+    sr = ars.s3_bucket
+    # sr = ars.secretsmanager_secret
+    # sr = ars.sfn_state_machine
+    # sr = ars.sfn_state_machine_execution
+    # sr = ars.sns_topic
+    # sr = ars.sqs_queue
+    # sr = ars.ssm_parameter
     # --------------------------------------------------------------------------
     res = sr.search(
         bsm=bsm,
@@ -28,8 +58,9 @@ if __name__ == "__main__":
         refresh_data=refresh_data,
         verbose=verbose,
     )
-    print(type(res[0]))
-    print(res[0])
+
+    # print(res[0].raw_data) # this is for type hint test
+
     if isinstance(res, dict):
         hits = res.pop("hits")
         docs = [hit["_source"] for hit in hits]
