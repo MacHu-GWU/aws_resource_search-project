83fdc1251334a064e9d3361eb4cae64e261e0280
Sanhe <MacHu-GWU@users.noreply.github.com>
2024-01-17 00:52:48 -0500

    more test

--- aws_resource_search/items/info_item.py
+++ aws_resource_search/items/info_item.py
@@ -10,7 +10,7 @@ from .base_item import BaseArsItem
 
 
 @dataclasses.dataclass
-class InfoItem(BaseArsItem):
+class InfoItem(BaseArsItem): # pragma: no cover
     """
     Represent an item to show any information. Nothing would happen when user
     press any of the user action key. This is usually used to show some helpful
--- tests/test_items_base_item.py
+++ tests/test_items_base_item.py
@@ -6,14 +6,14 @@ from aws_resource_search.items.base_item import BaseArsItem
 
 class TestBaseArsItem:
     def test(self):
-        """
-        We only test these two action manually on developer's machine, not in CI.
-        """
         url = "https://www.google.com"
         text = "a1b2c3"
         ui = zelfred.UI(handler=lambda x: x)
 
         item = BaseArsItem(title="hello")
+        # ----------------------------------------------------------------------
+        # We only test these two action manually on developer's machine, not in CI.
+        # ----------------------------------------------------------------------
         # item.open_url_or_print(ui=ui, url=url)
         # item.copy_or_print(ui=ui, text=text)
 
